9fc0cabec392c9cb9eaac901abc37408217d1fa6
Kruthibharath <kruthi.bharath@gmail.com>
2023-11-08 17:13:33 +0000

    feat: testing for rover to be within the plateau grid

--- src/plateaus/plateau.ts
+++ src/plateaus/plateau.ts
@@ -7,3 +7,7 @@ type direction = "N" | "S" | "E" | "W";
 export function createGrid(width: number, height: number): coordinates {
   return { x: width, y: height };
 }
+
+export function isWithInGrid(grid: coordinates, point: coordinates): boolean {
+  return point.x >= 0 && point.x <= grid.x && point.y >= 0 && point.y <= grid.y;
+}
--- src/rovers/rover.test.ts
+++ src/rovers/rover.test.ts
@@ -1,8 +1,26 @@
-import { createGrid } from "../plateaus/plateau";
+import { createGrid, isWithInGrid } from "../plateaus/plateau";
 import { MarsRover } from "../rovers/rover";
 describe("create a grid with given width and height", () => {
   test("should return { x: 5, y: 5 } for createGrid(5, 5)", () => {
     const grid = createGrid(5, 5);
     expect(grid).toEqual({ x: 5, y: 5 });
+  });
+});
+describe("isWithInGrid", () => {
+  test("check for rover tobe with in the plateau bounds", () => {
+    const grid = createGrid(5, 5);
+    expect(isWithInGrid(grid, { x: 3, y: 2 })).toBe(true);
+  });
+  test("check for rover reaching out of the plateau bounds with width", () => {
+    const grid = createGrid(5, 5);
+    expect(isWithInGrid(grid, { x: 6, y: 2 })).toBe(false);
+  });
+  test("check for rover reaching out of the plateau bounds with height", () => {
+    const grid = createGrid(5, 5);
+    expect(isWithInGrid(grid, { x: 2, y: 7 })).toBe(false);
+  });
+  test("check for rover reaching out of the plateau bounds with height", () => {
+    const grid = createGrid(5, 5);
+    expect(isWithInGrid(grid, { x: 8, y: 7 })).toBe(false);
   });
 });
