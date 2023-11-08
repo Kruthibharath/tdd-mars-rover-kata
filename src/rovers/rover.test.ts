dd3a2bde857745428caddc1c6b7ceb11854cdbc1
Kruthibharath <kruthi.bharath@gmail.com>
2023-11-08 22:43:20 +0000

    feat: implemented turn left and turn right functionality

--- src/rovers/rover.test.ts
+++ src/rovers/rover.test.ts
@@ -1,5 +1,5 @@
 import { createGrid, isWithInGrid } from "../plateaus/plateau";
-import { MarsRover } from "../rovers/rover";
+import { turnLeft, turnRight } from "../rovers/rover";
 describe("create a grid with given width and height", () => {
   test("should return { x: 5, y: 5 } for createGrid(5, 5)", () => {
     const grid = createGrid(5, 5);
@@ -22,5 +22,23 @@ describe("isWithInGrid", () => {
   test("check for rover reaching out of the plateau bounds with height", () => {
     const grid = createGrid(5, 5);
     expect(isWithInGrid(grid, { x: 8, y: 7 })).toBe(false);
+  });
+});
+
+describe("turnLeft", () => {
+  test("rover should turn left correctly for the given coordinate", () => {
+    expect(turnLeft("N")).toBe("W");
+    expect(turnLeft("W")).toBe("S");
+    expect(turnLeft("S")).toBe("E");
+    expect(turnLeft("E")).toBe("N");
+  });
+});
+
+describe("turnRight", () => {
+  test("rover should turn right correctly for the given coordinate", () => {
+    expect(turnRight("N")).toBe("E");
+    expect(turnRight("W")).toBe("N");
+    expect(turnRight("S")).toBe("W");
+    expect(turnRight("E")).toBe("S");
   });
 });
--- src/rovers/rover.ts
+++ src/rovers/rover.ts
@@ -1,3 +1,32 @@
-import { createGrid } from "../plateaus/plateau";
+//import { createGrid } from "../plateaus/plateau";
 
-export function MarsRover() {}
+type directions = "N" | "S" | "E" | "W";
+
+export function turnLeft(direction: directions): directions {
+  switch (direction) {
+    case "N":
+      return "W";
+    case "W":
+      return "S";
+    case "S":
+      return "E";
+    case "E":
+      return "N";
+    default:
+      return direction;
+  }
+}
+export function turnRight(direction: directions): directions {
+  switch (direction) {
+    case "N":
+      return "E";
+    case "W":
+      return "N";
+    case "S":
+      return "W";
+    case "E":
+      return "S";
+    default:
+      return direction;
+  }
+}
